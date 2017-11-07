f2d2a1accd8544c1e659f951ac12e2af3ede6411
americanmouths <shiverr@gmail.com>
2017-11-06 19:17:11 -0800

    update requirements

--- lib/sd_events.rb
+++ lib/sd_events.rb
@@ -1,5 +1,11 @@
+#require 'open-uri'
+require 'pry'
+#require 'nokogiri'
+
 require_relative "./sd_events/version"
 require_relative './sd_events/cli'
+require_relative './sd_events/events'
+
 
 module SdEvents
   # Your code goes here...
