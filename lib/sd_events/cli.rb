5d75a38a4db5819c3a8d0f9c23feabb917653d45
americanmouths <shiverr@gmail.com>
2017-11-09 19:22:15 -0800

    update menu, create and display event methods

--- lib/sd_events/cli.rb
+++ lib/sd_events/cli.rb
@@ -1,39 +1,55 @@
 class SdEvents::CLI
 
   def call
-    list_events
-    menu
-    goodbye
+    puts ""
+    puts "******* Events Happening in San Diego Today **********"
+    puts ""
+    list_menu
   end
 
-  def list_events
+  def list_menu
     puts ""
-    puts "****** Now Happening in San Diego *******"
-    puts ""
+    puts "Hello, the time is now #{Time.now}. Would you like to see:"
+    puts "1. Morning Events"
+    puts "2. Afternoon Events"
+    puts "3. Evening Events"
+    puts = ""
+
+    input = gets.strip.downcase
+    if input == "1"
+      create_morning_events
+      display_events
+    elsif input == "2"
+      create_afternoon_events
+      display_events
+    elsif input == "3"
+      create_evening_events
+      display_events
+    else
+      "Not sure what you're asking, please enter a valid number choice."
+    end
+  end
+
+  def create_morning_events
+    event_array = SdEvents::Scraper.scrape_morning
+    SdEvents::Events.create_from_collection(event_array)
+  end
+
+  def create_afternoon_events
+    event_array = SdEvents::Scraper.scrape_afternoon
+    SdEvents::Events.create_from_collection(event_array)
+  end
+
+  def create_evening_events
+    event_array = SdEvents::Scraper.scrape_evening
+    SdEvents::Events.create_from_collection(event_array)
+  end
+
+  def display_events
     SdEvents::Events.all.each.with_index(1) do |event, i|
-      puts "#{i}. #{event.name} - #{event.venue} - #{event.time}"
+      puts "#{i}. #{event.name.upcase} - #{event.time} - #{event.venue}"
     end
   end
 
-  def menu
-    input = nil
-    while input != "exit"
-      puts "Enter the number of the event you would like more info on or type list to see the events again or type exit"
-      input = gets.strip.downcase
-
-      if input.to_i > 0
-        the_event = SdEvents::Events.all[input.to_i-1]
-        puts "#{the_event.name} - #{the_event.venue} - #{the_event.time}"
-      elsif input == "list"
-        list_events
-      else
-        puts "Not sure what you want, type list to see the events or type exit"
-    end
-  end
-end
-
-  def goodbye
-    puts "See you tomorrow for some more events."
-  end
 
 end
