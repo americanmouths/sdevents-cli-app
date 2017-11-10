607f78fad78d9281bc8188fd46a67a75db5c77a2
americanmouths <shiverr@gmail.com>
2017-11-09 19:28:10 -0800

    move creation of events into event class

--- lib/sd_events/cli.rb
+++ lib/sd_events/cli.rb
@@ -2,7 +2,7 @@ class SdEvents::CLI
 
   def call
     puts ""
-    puts "******* Events Happening in San Diego Today **********"
+    puts "******* Events Happening Right Now in San Diego **********"
     puts ""
     list_menu
   end
@@ -17,34 +17,19 @@ class SdEvents::CLI
 
     input = gets.strip.downcase
     if input == "1"
-      create_morning_events
+      SdEvents::Events.create_morning_events
       display_events
     elsif input == "2"
-      create_afternoon_events
+      SdEvents::Events.create_afternoon_events
       display_events
     elsif input == "3"
-      create_evening_events
+      SdEvents::Events.create_evening_events
       display_events
     else
       "Not sure what you're asking, please enter a valid number choice."
     end
   end
-
-  def create_morning_events
-    event_array = SdEvents::Scraper.scrape_morning
-    SdEvents::Events.create_from_collection(event_array)
-  end
-
-  def create_afternoon_events
-    event_array = SdEvents::Scraper.scrape_afternoon
-    SdEvents::Events.create_from_collection(event_array)
-  end
-
-  def create_evening_events
-    event_array = SdEvents::Scraper.scrape_evening
-    SdEvents::Events.create_from_collection(event_array)
-  end
-
+  
   def display_events
     SdEvents::Events.all.each.with_index(1) do |event, i|
       puts "#{i}. #{event.name.upcase} - #{event.time} - #{event.venue}"
