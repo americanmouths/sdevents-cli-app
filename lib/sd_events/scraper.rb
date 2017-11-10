c43f9d8ba71bd502608798460d00a5adf58c775e
americanmouths <shiverr@gmail.com>
2017-11-09 21:00:36 -0800

    fix bug with scrapper not scrapping all events

--- lib/sd_events/scraper.rb
+++ lib/sd_events/scraper.rb
@@ -3,11 +3,11 @@ class SdEvents::Scraper
   def self.scrape_morning
     doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
     morning_events = []
-    doc.css(".daypart_morning").each do |event|
-      event.css(".event_list").each do |attribute|
-        event_name = attribute.css(".title").first.css("h4").text
-        event_venue = attribute.css(".title").first.css("h5").text.strip
-        event_time = attribute.css(".time").first.text.strip
+    doc.css(".daypart_morning .event_list").each do |event|
+      event.css(".title").collect do |attribute|
+        event_name = attribute.css("h4").text.strip
+        event_venue = attribute.css("h5").text.strip
+        event_time = event.css(".time").first.text.strip
       morning_events << {name: event_name, venue: event_venue, time: event_time}
       end
     end
@@ -17,11 +17,11 @@ class SdEvents::Scraper
   def self.scrape_afternoon
     doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
     afternoon_events = []
-    doc.css(".daypart_afternoon").each do |event|
-      event.css(".event_list").each do |attribute|
-        event_name = attribute.css(".title").first.css("h4").text
-        event_venue = attribute.css(".title").first.css("h5").text.strip
-        event_time = attribute.css(".time").first.text.strip
+    doc.css(".daypart_afternoon .event_list").each do |event|
+      event.css(".title").collect do |attribute|
+        event_name = attribute.css("h4").text.strip
+        event_venue = attribute.css("h5").text.strip
+        event_time = event.css(".time").first.text.strip
       afternoon_events << {name: event_name, venue: event_venue, time: event_time}
       end
     end
@@ -31,12 +31,12 @@ class SdEvents::Scraper
   def self.scrape_evening
     doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
     evening_events = []
-    doc.css(".daypart_evening").each do |event|
-      event.css(".event_list").each do |attribute|
-        event_name = attribute.css(".title").first.css("h4").text
-        event_venue = attribute.css(".title").first.css("h5").text.strip
-        event_time = attribute.css(".time").first.text.strip
-      evening_events << {name: event_name, venue: event_venue, time: event_time}
+    doc.css(".daypart_evening .event_list").each do |event|
+      event.css(".title").collect do |attribute|
+        event_name = attribute.css("h4").text.strip
+        event_venue = attribute.css("h5").text.strip
+        event_time = event.css(".time").first.text.strip
+        evening_events << {name: event_name, venue: event_venue, time: event_time}
       end
     end
     evening_events
