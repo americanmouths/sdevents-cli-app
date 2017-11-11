61a07fb6ef120f88f86c8d5658e298d244c36931
americanmouths <shiverr@gmail.com>
2017-11-11 10:32:51 -0800

    change from hashes into event objects

--- lib/sd_events/scraper.rb
+++ lib/sd_events/scraper.rb
@@ -1,47 +1,40 @@
 class SdEvents::Scraper
 
-  def self.scrape_morning
-    doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
-    morning_events = []
-    doc.css(".daypart_morning .event_list").each do |event|
-      event.css(".title").collect do |attribute|
-        event_name = attribute.css("h4").text.strip
-        event_venue = attribute.css("h5").text.strip
-        event_time = event.css(".time").first.text.strip
-      morning_events << {name: event_name, venue: event_venue, time: event_time}
-      end
-    end
-    morning_events
+  def self.scrape
+    @@doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
   end
+
+  def self.scrape_morning
+    self.scrape.css(".daypart_morning .event_list").each do |event|
+      event.css(".title").collect do |attribute|
+        new_event = SdEvents::Events.new
+        new_event.name = attribute.css("h4").text.strip
+        new_event.venue = attribute.css("h5").text.strip
+        new_event.time = event.css(".time").first.text.strip
+    end
+  end
+end
 
   def self.scrape_afternoon
-    doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
-    afternoon_events = []
-    doc.css(".daypart_afternoon .event_list").each do |event|
+    self.scrape.css(".daypart_afternoon .event_list").each do |event|
       event.css(".title").collect do |attribute|
-        event_name = attribute.css("h4").text.strip
-        event_venue = attribute.css("h5").text.strip
-        event_time = event.css(".time").first.text.strip
-      afternoon_events << {name: event_name, venue: event_venue, time: event_time}
-      end
+        new_event = SdEvents::Events.new
+        new_event.name = attribute.css("h4").text.strip
+        new_event.venue = attribute.css("h5").text.strip
+        new_event.time = event.css(".time").first.text.strip
     end
-    afternoon_events
   end
+end
 
   def self.scrape_evening
-    doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
-    evening_events = []
-    doc.css(".daypart_evening .event_list").each do |event|
+    self.scrape.css(".daypart_evening .event_list").each do |event|
       event.css(".title").collect do |attribute|
-        event_name = attribute.css("h4").text.strip
-        event_venue = attribute.css("h5").text.strip
-        event_time = event.css(".time").first.text.strip
-        evening_events << {name: event_name, venue: event_venue, time: event_time}
-      end
+        new_event = SdEvents::Events.new
+        new_event.name = attribute.css("h4").text.strip
+        new_event.venue = attribute.css("h5").text.strip
+        new_event.time = event.css(".time").first.text.strip
     end
-    evening_events
   end
-
-
+end
 
 end
