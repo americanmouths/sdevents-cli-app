04b300c9480fa3833de183435365f9a4dab802b4
americanmouths <shiverr@gmail.com>
2017-11-11 10:34:20 -0800

    change events from hashes to objects

--- lib/sd_events/events.rb
+++ lib/sd_events/events.rb
@@ -2,34 +2,40 @@ class SdEvents::Events
   attr_accessor :name, :venue, :time
   @@all = []
 
-  def initialize(event_hash)
-    event_hash.each{|key, value| self.send(("#{key}="), value)}
+  def initialize
+    @name = name
+    @venue = venue
+    @time = time
     @@all << self
   end
 
-  def self.create_from_collection(events_array)
-    events_array.each do |event_hash|
-    self.new(event_hash)
-    end
+  def self.save
+    @@all << self
   end
 
+  #def self.create_from_collection(events_array)
+    #Event.new.tap do |event|
+    #events_array.each do |event_hash|
+      #event_hash.each do
+    #self.new(event_hash)
+    #end
+  #end
+
   def self.create_morning_events
-    event_array = SdEvents::Scraper.scrape_morning
-    self.create_from_collection(event_array)
+    SdEvents::Scraper.scrape_morning
   end
 
   def self.create_afternoon_events
-    event_array = SdEvents::Scraper.scrape_afternoon
-    self.create_from_collection(event_array)
+    SdEvents::Scraper.scrape_afternoon
   end
 
   def self.create_evening_events
-    event_array = SdEvents::Scraper.scrape_evening
-    self.create_from_collection(event_array)
+    SdEvents::Scraper.scrape_evening
   end
 
   def self.all
     @@all
   end
+
 
 end
