3e109bb9ab78f15af20c4a299f9fff27700d451d
americanmouths <shiverr@gmail.com>
2017-11-12 11:39:50 -0800

    fix bug with morning events

--- lib/sd_events/scraper.rb
+++ lib/sd_events/scraper.rb
@@ -1,24 +1,24 @@
 class SdEvents::Scraper
-  
+
   def self.scrape
     @@doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
   end
 
   def self.scrape_morning
-    self.scrape.css(".daypart_morning .event_list").each do |event|
+    self.scrape.css("div#body div#content.content div.daypart_morning .event_list").each do |event|
       event.css("tr").collect do |attribute|
         new_event = SdEvents::Events.new
         new_event.name = attribute.css(".title").css("h4").text.strip
         new_event.venue = attribute.css(".title").css("h5").text.strip
         new_event.time = attribute.css("td.time").text.strip
-        new_event.city = attribute.css("td.city ul li.last-child").text.strip
+        new_event.city = attribute.css("td.city").text.strip
         new_event.category = attribute.css("td.category").text.strip
       end
     end
   end
 
   def self.scrape_afternoon
-    self.scrape.css(".daypart_afternoon .event_list").each do |event|
+    self.scrape.css("div.daypart_afternoon .event_list").each do |event|
       event.css("tr").collect do |attribute|
         new_event = SdEvents::Events.new
         new_event.name = attribute.css(".title").css("h4").text.strip
@@ -31,7 +31,7 @@ class SdEvents::Scraper
   end
 
   def self.scrape_evening
-    self.scrape.css(".daypart_evening .event_list").each do |event|
+    self.scrape.css("div.daypart_evening .event_list").each do |event|
       event.css("tr").collect do |attribute|
         new_event = SdEvents::Events.new
         new_event.name = attribute.css(".title").css("h4").text.strip
@@ -44,7 +44,7 @@ class SdEvents::Scraper
   end
 
   def self.scrape_night
-    self.scrape.css(".daypart_night .event_list").each do |event|
+    self.scrape.css("div.daypart_night .event_list").each do |event|
       event.css("tr").collect do |attribute|
         new_event = SdEvents::Events.new
         new_event.name = attribute.css(".title").css("h4").text.strip
