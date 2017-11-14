93d3f481ff4a637f2e173f55548e2ee3eae79e30
americanmouths <shiverr@gmail.com>
2017-11-13 20:01:21 -0800

    update with new version

--- lib/sd_events.rb
+++ lib/sd_events.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'pry'
 require 'nokogiri'
 
-require_relative "./sd_events/version"
+require_relative './sd_events/version'
 require_relative './sd_events/cli'
 require_relative './sd_events/events'
 require_relative './sd_events/scraper'
