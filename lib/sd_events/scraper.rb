15ba050bfba3a88768c64761d0905c8327439c08
americanmouths <shiverr@gmail.com>
2017-11-14 18:48:22 -0800

    remove unnecessary class variable in scrape method

--- lib/sd_events/scraper.rb
+++ lib/sd_events/scraper.rb
@@ -1,7 +1,7 @@
 class SdEvents::Scraper
 
   def self.scrape
-    @@doc = Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
+    Nokogiri::HTML(open("https://www.sandiegoreader.com/events/#"))
   end
 
   def self.scrape_events(attribute)
